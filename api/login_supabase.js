ba520904831fb7fef39164afd70c1346c9940dd2
Virqy Apriyadi Jihad <86461430+kikok456@users.noreply.github.com>
2026-05-12 15:08:47 +0700

    Update login_supabase.js

--- api/login_supabase.js
+++ api/login_supabase.js
@@ -19,12 +19,34 @@ const supabase = createClient(
 
 
 
-// LOGIN API
-module.exports = async (req, res) => {
+export default async function handler(
+  req,
+  res
+) {
+
+  // CORS
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, OPTIONS"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type"
+  );
+
+  // handle preflight
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
   // hanya POST
   if (req.method !== "POST") {
-
     return res.status(405).json({
       message: "Method not allowed",
     });
