5ec75e7cfae4c94954071ac238d0c25e47e4c466
Virqy Apriyadi Jihad <86461430+kikok456@users.noreply.github.com>
2026-05-13 13:20:22 +0700

    Update user_supabase.js

--- api/user_supabase.js
+++ api/user_supabase.js
@@ -14,7 +14,20 @@ const supabase = createClient(
 
 
 // GET USERS
+
 module.exports = async (req, res) => {
+
+  // =========================
+  // CORS
+  // =========================
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // handle preflight
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
   // hanya GET
   if (req.method !== "GET") {
