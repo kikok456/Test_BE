bf60b1de61683e926fdcc69693b09f217cdbee0b
Virqy Apriyadi Jihad <86461430+kikok456@users.noreply.github.com>
2026-05-26 15:28:40 +0700

    Update lampung_raw_supabase.js

--- api/lampung_raw_supabase.js
+++ api/lampung_raw_supabase.js
@@ -1,19 +1,39 @@
 require("dotenv").config();
 
-const {
-  createClient,
-} = require("@supabase/supabase-js");
+const { createClient } =
+  require("@supabase/supabase-js");
 
-const supabase =
-  createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
-  );
 
-module.exports = async (
-  req,
-  res
-) => {
+
+// koneksi Supabase
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
+
+module.exports = async (req, res) => {
+
+  // =========================
+  // CORS
+  // =========================
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // handle preflight
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+
+  // hanya POST
+  if (req.method !== "GET") {
+
+    return res.status(405).json({
+      message: "Method not allowed",
+    });
+  }
 
   try {
 
